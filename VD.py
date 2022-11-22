69eaaf180b8f3f7e1546287bf2b478411a066fdd
mr. whalen <michael.whalen@sitechhs.com>
2022-11-22 10:25:50 -0500

    sdfghdfhjfdghjfgh

---
+++ VD.py
@@ -0,0 +1,48 @@
+const data = [
+  {
+    city: "Moscow",
+    temperature: "17 °C",
+    wind: 4,
+    description: "Partly cloudy",
+    forecast: [18, 22, 23],
+  },
+  {
+    city: "New York",
+    temperature: "21 °C",
+    wind: 20,
+    description: "Partly cloudy",
+    forecast: [19, 14, 11],
+  },
+  {
+    city: "Miami",
+    temperature: "+28 °C",
+    wind: 28,
+    description: "Rain",
+    forecast: [28, 27, 28],
+  },
+  {
+    city: "Helsinki",
+    temperature: "17 °C",
+    wind: 4,
+    description: "Sunny",
+    forecast: [18, 20, 25],
+  },
+  {
+    city: "Trenton",
+    temperature: "21 °C",
+    wind: 18,
+    description: "Partly cloudy",
+    forecast: [20, 26, 11],
+  },
+  {
+    city: "Tampa Bay",
+    temperature: "+28 °C",
+    wind: 26,
+    description: "Rain",
+    forecast: [24, 20, 23],
+  },
+];
+
+// 1)While chaining array methods, print the cities that have wind greater than 9 miles an hour
+
+// 2) While chaining array methods, print the name of each city that has a forecast later this week that includes a temperature that is exactly 20
